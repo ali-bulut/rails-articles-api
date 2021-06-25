979d801459eb63a486d9aaa76381914d7e822e21
Ali Bulut <ali@alibulut.net>
2021-06-25 18:13:11 +0300

    create post request added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,12 @@ class ArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: serializer.new(article)
-  # rescue ActiveRecord::RecordNotFound => e
-  #   render json: { message: e.message, detail: "Formatted response" }
+    # rescue ActiveRecord::RecordNotFound => e
+    #   render json: { message: e.message, detail: "Formatted response" }
+  end
+
+  def create
+
   end
 
   def serializer
